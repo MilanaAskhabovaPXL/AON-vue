633177c30e178390fcd2dd33cae7440ef92b11e1
arnobarzan <arno.barzan@pxl.be>
2024-02-18 15:39:02 +0100

    Adds samples for the first lesson

---
+++ lesson01_samples/CounterComponent.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h1>Quentin Tarantino Movies Counter</h1>
+    <p>Number of movies: {{ count }}</p>
+    <button @click="incrementCount">Increment</button>
+  </div>
+</template>
+
+<script setup>
+let count = 0;
+
+function incrementCount() {
+    count.value++;
+}
+
+</script>
+
+<style scoped>
+
+h1 {
+  font-family: fantasy;
+}
+
+button {
+  padding: 8px 16px;
+  font-size: 16px;
+  background-color: #007bff;
+  color: #fff;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+
+</style>
---
+++ lesson01_samples/MyFirstComponent.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+        <!-- <h1>{{ title }}</h1> -->
+        
+        <!-- <p>The time is {{ timeInHours }} : {{ timeInMinutes }}</p>
+            
+            <p>Next hour = {{ timeInHours + 1 }}</p>
+            
+            <p>Good {{ timeInHours > 18 ? 'EVENING' : 'MORNING' }}</p>
+            
+            <p>{{ title.split('').reverse().join('') }}</p> -->
+            
+        <!-- <a :href="vueJsUrl">VueJs</a> -->
+        
+        <!-- <div>
+            <p v-if="timeInHours < 12">Good morning</p>
+            <p v-else>Hello!</p>
+        </div> -->
+
+        <!-- <h2>Movie characters:</h2>
+        <ul>
+            <li v-for="character in movieCharacters" :key="character.id">{{ character }}</li>
+        </ul> -->
+    </div>
+</template>
+
+<script setup>
+
+
+//const title = 'My First Component';
+//const timeInHours = new Date().getHours();
+// const timeInMinutes = new Date().getMinutes();
+
+// const vueJsUrl = 'https://vuejs.org/';	
+// const movieCharacters = [
+//     {id:0, name: "Vincent Vega"},
+//     {id: 1, name:"Jules Winnfield"},
+//     {id: 2, name:"Shosanna Dreyfus"}
+// ];
+
+
+</script>
+
+<style>
+</style>
+
